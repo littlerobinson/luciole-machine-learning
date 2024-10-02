7e794a5e8c0f187739fd610acbb0b1baa4a85e6f
alexandre <2a.alexandre@gmail.com>
2024-10-02 18:49:40 +0200

    feat: add tool class for text processing in NLP

---
+++ src/class/language/language_tools.py
@@ -0,0 +1,103 @@
+import re
+import string
+import unicodedata
+import contractions
+
+
+class TextProcessing:
+    """
+    The TextProcessing class is designed to provide various text processing methods.
+    It includes functionalities to clean, standardize, and transform text to prepare it
+    for further analysis, such as natural language processing (NLP).
+    """
+
+    def sanitize(self, input_text):
+        """
+        This method takes raw text as input and applies a series of transformations to clean it.
+        The cleaning steps include:
+        1. Converting all characters to lowercase.
+        2. Removing all occurrences of HTML tags'.
+        3. Removing mentions (@) and hashtags (#).
+        4. Removing punctuation.
+        """
+        # Transform all characters to lowercase
+        sanitize_text = self.to_lowercase(input_text)
+
+        # Remove all occurrences of the string '<br />'
+        sanitize_text = self.strip_html(sanitize_text)
+
+        # Remove mentions (@) and hashtags (#)
+        sanitize_text = self.remove_mentions_and_tags(sanitize_text)
+
+        # Remove punctuation
+        sanitize_text = self.remove_punctuation(sanitize_text)
+
+    def to_lowercase(text):
+        """
+        This function takes a text as input and returns the text with all characters converted to lowercase.
+        """
+        return text.lower()
+
+    def standardize_accented_chars(text):
+        """
+        This function takes a text as input and returns the text with accented characters standardized.
+        """
+        return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+
+    def strip_html(text):
+        """
+        This function takes a text as input and returns the text with all HTML tags removed.
+        """
+        return re.sub('<[^<]+?>', '', text)
+
+    def remove_url(text):
+        """
+        This function takes a text as input and returns the text with all URLs removed.
+        """
+        return re.sub(r'https?:\S*', '', text)
+
+    def remove_mentions_and_tags(text):
+        """
+        This function takes a text as input and returns the text with all mentions (@) and hashtags (#) removed.
+        """
+        text = re.sub(r'@\S*', '', text)
+        return re.sub(r'#\S*', '', text)
+
+    def remove_special_characters(text):
+        """
+        This function takes a text as input and returns the text with all special characters removed.
+        """
+        pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]'
+        return re.sub(pat, '', text)
+
+    def remove_numbers(text):
+        """
+        This function takes a text as input and returns the text with all numbers removed.
+        """
+        pattern = r'[^a-zA-z.,!?/:;\"\'\s]'
+        return re.sub(pattern, '', text)
+
+    def remove_punctuation(text):
+        """
+        This function takes a text as input and returns the text with all punctuation removed.
+        """
+        return ' '.join([c for c in text if c not in string.punctuation])
+
+    def expand_contractions(text):
+        """
+        This function takes a text as input and returns the text with all contractions expanded.
+        """
+        expanded_words = []
+        for word in text.split():
+            expanded_words.append(contractions.fix(word))
+        return ' '.join(expanded_words)
+
+    def lemmatize(text, nlp):
+        """
+        This function takes a text and a language processing object (nlp) as input and returns the text with all words lemmatized.
+        """
+        doc = nlp(text)
+        lemmatized_text = []
+        for token in doc:
+            lemmatized_text.append(token.lemma_)
+        return ' '.join(lemmatized_text)
